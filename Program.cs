d15440b3b6e4329a56479226e0f5b5b41711a880
Richardjcs <r.cacuango@gmail.com>
2017-07-13 22:56:42 +0200

    Console program dotnet

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
+//Namespace
 namespace csharp
 {
+    //Class Name
     class Program
     {
+        //Entry Point Method
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //start here let's code !
+           
+            int age = 23;
+            string appName = "DotNet";
+            string appVersion = "1.0.0";
+            string appAuthor = "Richard Cacuango";
+
+            //Change text color
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //write out app info
+            Console.WriteLine("{0}: Version {1} by {2}, {3}",appName,appVersion,appAuthor,age);
+
+            //Reset color
+            Console.ResetColor();
+            //Ask user name
+            Console.WriteLine("What's your name?");
+            //Get user input
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}",input);
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # csharp
 c# In 60 minutes
+
+
+## Using dotnet console 
+
+Create project with **dotnet**
+
+```
+$ dotnet new console
+$ dotnet restore
+$ dotnet run
+```
